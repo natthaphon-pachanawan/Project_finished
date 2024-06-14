e5329fe08ab58034833ac264a62401c1ec560ae2
Combo0445 <149581752+Combo0445@users.noreply.github.com>
2024-06-14 13:18:31 +0700

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,18 +11,18 @@ class UserController extends Controller
 {
     public function addUser()
     {
-        // $personnel = Personnel::where('Type_Personnel', 'Admin')->first();
-        // if (!$personnel) {
-        //     return response()->json(['message' => 'Personnel ไม่พบ'], 404);
-        // }
-        // $user = new User();
-        // $user->Username = 'admin';
-        // $user->Email = 'admin@gmail.com';
-        // $user->Password = Hash::make('admin');
-        // $user->ID_Personnel = $personnel->ID_Personnel;
-        // $user->Type_Personnel = 'Admin';
-        // $user->save();
-        // return response()->json(['message' => 'User ถูกเพิ่มเรียบร้อยแล้ว'], 201);
+        $personnel = Personnel::where('Type_Personnel', 'Admin')->first();
+        if (!$personnel) {
+            return response()->json(['message' => 'Personnel ไม่พบ'], 404);
+        }
+        $user = new User();
+        $user->Username = 'admin';
+        $user->Email = 'admin@gmail.com';
+        $user->Password = Hash::make('admin');
+        $user->ID_Personnel = $personnel->ID_Personnel;
+        $user->Type_Personnel = 'Admin';
+        $user->save();
+        return response()->json(['message' => 'User ถูกเพิ่มเรียบร้อยแล้ว'], 201);
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         // $personnel = Personnel::where('Type_Personnel', 'Staff')->first();
         // if (!$personnel) {
@@ -37,17 +37,17 @@ class UserController extends Controller
         // $user->save();
         // return response()->json(['message' => 'User ถูกเพิ่มเรียบร้อยแล้ว'], 201);
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        $personnel = Personnel::where('Type_Personnel', 'Doctor')->first();
-        if (!$personnel) {
-            return response()->json(['message' => 'Personnel ไม่พบ'], 404);
-        }
-        $user = new User();
-        $user->Username = 'doctor';
-        $user->Email = 'doctor@gmail.com';
-        $user->Password = Hash::make('doctor');
-        $user->ID_Personnel = $personnel->ID_Personnel;
-        $user->Type_Personnel = 'Doctor';
-        $user->save();
-        return response()->json(['message' => 'User ถูกเพิ่มเรียบร้อยแล้ว'], 201);
+        // $personnel = Personnel::where('Type_Personnel', 'Doctor')->first();
+        // if (!$personnel) {
+        //     return response()->json(['message' => 'Personnel ไม่พบ'], 404);
+        // }
+        // $user = new User();
+        // $user->Username = 'doctor';
+        // $user->Email = 'doctor@gmail.com';
+        // $user->Password = Hash::make('doctor');
+        // $user->ID_Personnel = $personnel->ID_Personnel;
+        // $user->Type_Personnel = 'Doctor';
+        // $user->save();
+        // return response()->json(['message' => 'User ถูกเพิ่มเรียบร้อยแล้ว'], 201);
     }
 }
