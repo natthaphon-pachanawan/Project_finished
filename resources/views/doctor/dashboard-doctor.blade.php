5cfdd4c8d0e7a3815f730e118a25dc59ed198116
Combo0445 <149581752+Combo0445@users.noreply.github.com>
2024-06-20 05:36:37 +0700

    doctor/update

--- resources/views/doctor/dashboard-doctor.blade.php
+++ resources/views/doctor/dashboard-doctor.blade.php
@@ -5,9 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+         .sidebar {
+            width: 200px;
+            background-color: #333;
+            height: 100vh;
+            position: fixed;
+            top: 60px; /* Height of the navbar */
+            left: 0;
+            padding-top: 20px;
+        }
+        .sidebar ul a {
+            color: #ecf0f1;
+            padding-top: 20px;
+            text-decoration: none;
+            display: block;
+        }
+
+    </style>
 </head>
 <body>
     @include('layout.nav')
+    
+    <div class="sidebar">
+        <ul>
+        <a href="#">ข้อมูลADL</a>
+        <a href="#">ข้อมูลCG</a>
+        <a href="#">แนะนำวิธีรักษา</a>
+    </ul>
+    </div>
 </body>
 </html>
 
